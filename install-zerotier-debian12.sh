1423c10d2f89a9b891ac9dc44abf194816f4ed45
Luís Gabriel Tavares Ferreira <luis.gabriel@unemat.br>
2025-08-15 10:24:26 -0400

    Add files via upload
    This script installs Zerotier on Debian 12

---
+++ install-zerotier-debian12.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+wget -P /opt https://download.zerotier.com/RELEASES/1.14.2/dist/debian/bookworm/zerotier-one_1.14.2_amd64.deb
+
+dpkg -i zerotier-one_1.14.2_amd64.deb
+
+systemctl start zerotier-one
+
+systemctl enable zerotier-one 
+
+#To connect a machine to a ZeroTier network
+zerotier-cli join <networkID>
+
+#Leaving a ZeroTier Network
+#zerotier-cli leave <networkID>
+rm -rf /opt/zerotier-one*
+
+#ZeroTier CLI Commands
+#zerotier-cli status
+#systemctl status zerotier-one
+#zerotier-cli listnetworks
+exit 0
