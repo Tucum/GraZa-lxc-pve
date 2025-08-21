8b5ddb8717063666ff2444c55174ff7bc903dc05
Luís Gabriel Tavares Ferreira <luis.gabriel@unemat.br>
2025-08-21 08:24:40 -0400

    Update install-zabbix-debian12.sh

--- scripts/install-zabbix-debian12.sh
+++ scripts/install-zabbix-debian12.sh
@@ -86,3 +86,9 @@ update-locale LANG=en_US.UTF-8
 locale
 
 systemctl restart zabbix-server zabbix-agent apache2
+
+#RESOLVER ERRO MEMORIA
+wget https://raw.githubusercontent.com/kvaps/zabbix-linux-container-template/master/zabbix_container.conf -O /etc/zabbix/zabbix_agentd.d/zabbix_container.conf
+
+#importar template do agent do zabbix
+#alterar configuração do widget do dashboard principal para ct.memory.size[pused]
